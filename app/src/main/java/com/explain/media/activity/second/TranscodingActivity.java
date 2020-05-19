ba670736d7d8e9dde1e0671242a9ff44681c97b7
panbeixing <panbx@aheading.com>
2020-05-19 13:37:24 +0800

    '添加文件类型判断'

--- app/src/main/java/com/explain/media/activity/second/TranscodingActivity.java
+++ app/src/main/java/com/explain/media/activity/second/TranscodingActivity.java
@@ -3,16 +3,19 @@ package com.explain.media.activity.second;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.explain.media.R;
 import com.explain.media.activity.BaseActivity;
+import com.explain.media.utils.MediaFile;
 import com.explain.media.utils.SDFileUtil;
 
 /**
  * 转码
  */
 public class TranscodingActivity extends BaseActivity implements View.OnClickListener {
+    private TextView tvFilePath;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +23,7 @@ public class TranscodingActivity extends BaseActivity implements View.OnClickLis
 
         setContentView(R.layout.activity_transcoding);
         findViewById(R.id.tv_select_file).setOnClickListener(this);
+        tvFilePath = findViewById(R.id.tv_filepath);
     }
 
     @Override
@@ -34,9 +38,11 @@ public class TranscodingActivity extends BaseActivity implements View.OnClickLis
     @Override
     protected void onSelectedFile(String filePath) {
         super.onSelectedFile(filePath);
-        if (!"audio".equals(SDFileUtil.getFileType(filePath))) {
+        if (!MediaFile.isAudioFileType(filePath)) {
             Toast.makeText(this, "文件格式错误，非音频文件", Toast.LENGTH_SHORT).show();
             return;
         }
+
+        tvFilePath.setText("文件地址:" + filePath);
     }
 }
